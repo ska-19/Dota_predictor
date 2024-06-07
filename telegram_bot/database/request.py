bce646d294503e2f526fccbf93702cbe497b916d
Семенкович Кирилл Александрович <kasemenkovich@edu.hse.ru>
2024-06-07 14:28:52 +0300

    2.0

--- telegram_bot/database/request.py
+++ telegram_bot/database/request.py
@@ -3,7 +3,7 @@ import joblib
 import pandas as pd
 import xgboost as xgb
 
-
+from  handlers.pick_handlers import dota_heroes
 async def set_user(tg_id:int, user_data: dict):
     Users.create_user(tg_id, user_data['username'])
     user = Users.get_user(tg_id)
@@ -28,10 +28,26 @@ async def reg_user(user_data: dict):
 
 async def get_prediction(req_data: dict):
     model_file_path = './database/xgboost_model_pipeline.joblib'
+    encoder_file_path = './database/onehot_encoder.joblib'
     loaded_model_pipeline = joblib.load(model_file_path)
+    loaded_encoder = joblib.load(encoder_file_path)
+    first_five_cols = ['0', '1', '2', '3', '4']
+    last_five_cols = ['5', '6', '7', '8', '9']
+
     new_data = {}
     for key, value in req_data.items():
         new_data[str(int(str(key)[1:])-1)] = value
     new_data_df = pd.DataFrame([new_data])
-    prediction = loaded_model_pipeline.predict_proba(new_data_df)[:, 1]
+
+    new_first_five_encoded = loaded_encoder.transform(new_data_df[first_five_cols].values.reshape(-1, 1)).reshape(
+        new_data_df.shape[0], -1, len(loaded_encoder.categories_[0])).sum(axis=1)
+    new_last_five_encoded = loaded_encoder.transform(new_data_df[last_five_cols].values.reshape(-1, 1)).reshape(
+        new_data_df.shape[0], -1, len(loaded_encoder.categories_[0])).sum(axis=1)
+
+    new_combined_encoded = new_first_five_encoded - new_last_five_encoded
+
+    encoded_columns = [f'hero_{cat}' for cat in dota_heroes]
+    new_encoded_df = pd.DataFrame(new_combined_encoded, columns=encoded_columns)
+
+    prediction = loaded_model_pipeline.predict_proba(new_encoded_df)[:, 1]
     return prediction
--- telegram_bot/handlers/pick_handlers.py
+++ telegram_bot/handlers/pick_handlers.py
@@ -12,19 +12,22 @@ router = Router()
 
 dota_heroes = ['abaddon', 'alchemist', 'ancient_apparition', 'anti_mage', 'arc_warden', 'axe', 'bane', 'batrider',
                'beastmaster', 'bloodseeker', 'bounty_hunter', 'brewmaster', 'bristleback', 'broodmother',
-               'centaur_warrunner', 'chaos_knight', 'chen', 'clinkz', 'clockwerk', 'crystal_maiden', 'dark_seer',
-               'dazzle', 'death_prophet', 'disruptor', 'doom', 'dragon_knight', 'drow_ranger', 'earth_spirit',
-               'earthshaker', 'elder_titan', 'ember_spirit', 'enchantress', 'enigma', 'faceless_void', 'gyrocopter',
-               'huskar', 'invoker', 'io', 'jakiro', 'juggernaut', 'keeper_of_the_light', 'kunkka', 'legion_commander',
-               'leshrac', 'lich', 'lifestealer', 'lina', 'lion', 'lone_druid', 'luna', 'lycan', 'magnus', 'medusa',
-               'meepo', 'mirana', 'morphling', 'naga_siren', 'natures_prophet', 'necrophos', 'night_stalker',
-               'nyx_assassin', 'ogre_magi', 'omniknight', 'oracle', 'outworld_devourer', 'phantom_assassin',
-               'phantom_lancer', 'phoenix', 'puck', 'pudge', 'pugna', 'queen_of_pain', 'razor', 'riki', 'rubick',
-               'sand_king', 'shadow_demon', 'shadow_fiend', 'shadow_shaman', 'silencer', 'skywrath_mage', 'slardar',
-               'slark', 'sniper', 'spectre', 'spirit_breaker', 'storm_spirit', 'sven', 'techies', 'templar_assassin',
-               'terrorblade', 'tidehunter', 'timbersaw', 'tinker', 'tiny', 'treant_protector', 'troll_warlord', 'tusk',
-               'underlord', 'undying', 'ursa', 'vengeful_spirit', 'venomancer', 'viper', 'visage', 'warlock', 'weaver',
-               'windranger', 'winter_wyvern', 'witch_doctor', 'wraith_king', 'zeus']
+               'centaur_warrunner', 'chaos_knight', 'chen', 'clinkz', 'clockwerk', 'crystal_maiden',
+               'dark_seer', 'dark_willow', 'dawnbreaker', 'dazzle', 'death_prophet', 'disruptor', 'doom',
+               'dragon_knight', 'drow_ranger', 'earth_spirit', 'earthshaker', 'elder_titan', 'ember_spirit',
+               'enchantress', 'enigma', 'faceless_void', 'grimstroke', 'gyrocopter', 'hoodwink', 'huskar',
+               'invoker', 'io', 'jakiro', 'juggernaut', 'keeper_of_the_light', 'kunkka', 'legion_commander',
+               'leshrac', 'lich', 'lifestealer', 'lina', 'lion', 'lone_druid', 'luna', 'lycan', 'magnus', 'marci',
+               'mars', 'medusa', 'meepo', 'mirana', 'monkey_king', 'morphling', 'muerta', 'naga_siren',
+               'natures_prophet', 'necrophos', 'night_stalker', 'nyx_assassin', 'ogre_magi', 'omniknight', 'oracle',
+               'outworld_destroyer', 'pangolier', 'phantom_assassin', 'phantom_lancer', 'phoenix', 'primal_beast',
+               'puck', 'pudge', 'pugna', 'queen_of_pain', 'razor', 'riki', 'rubick', 'sand_king', 'shadow_demon',
+               'shadow_fiend', 'shadow_shaman', 'silencer', 'skywrath_mage', 'slardar', 'slark', 'snapfire', 'sniper',
+               'spectre', 'spirit_breaker', 'storm_spirit', 'sven', 'techies', 'templar_assassin', 'terrorblade',
+               'tidehunter', 'timbersaw', 'tinker', 'tiny', 'treant_protector', 'troll_warlord', 'tusk', 'underlord',
+               'undying', 'ursa', 'vengeful_spirit', 'venomancer', 'viper', 'visage', 'void_spirit', 'warlock',
+               'weaver', 'windranger', 'winter_wyvern', 'witch_doctor', 'wraith_king', 'zeus']
+
 
 
 class PickState(StatesGroup):
